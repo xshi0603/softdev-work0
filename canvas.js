e720685080999cb8047d91e2e8c5fa914dad9c7b
Xing Tao Shi <xshi@stuy.edu>
2018-02-11 03:20:54 -0500

    fix

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 var c = document.getElementById("board");
 var ctx = c.getContext("2d");
 
-var tbutton = document.getElementById("tbutton");
+//var tbutton = document.getElementById("tbutton");
 var cbutton = document.getElementById("cbutton");
 
 var toggled = false;//false == squares
@@ -15,21 +15,31 @@ tbutton.addEventListener('click', function() {
 cbutton.addEventListener('click', function() {
 
 	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.beginPath();
 
     });
 
 c.addEventListener('click', function(e) {
 	
-	if (toggled) {
-	    ctx.beginPath();
-	    ctx.arc(e.offsetX, e.offsetY, 30, 0, 2 * Math.PI);
-	    ctx.stroke();
-	    ctx.fill();
-	}
+	//if (toggled) {
+	//ctx.beginPath();
+	ctx.lineTo(e.offsetX, e.offsetY);//makes a line
+	ctx.stroke();//draws the line
+
+	ctx.beginPath();
+	ctx.arc(e.offsetX, e.offsetY, 15, 0, 2 * Math.PI);//makes circle
+	ctx.fill();//draws circle
+
+	ctx.beginPath();//start path
+	ctx.moveTo(e.offsetX, e.offsetY);//tracks old xy
+
+	/*
+}
 	else {
 	    ctx.fillRect(e.offsetX-25, e.offsetY-25, 50, 50);
 	}
-
-	console.log(e);
-
+	*/
+	//console.log(e);
+	//console.log(ctx);
+	
     });
